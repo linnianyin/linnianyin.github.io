1eeab94aef3a0fb94e11cf56aa9279bc6fa18445
sreed <youaresobad@foxmail.com>
2020-08-19 10:56:16 +0800

    增加水波和波光效果

---
+++ shader-2.4.0-demo/src/settings.108b3.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"23624",Shader:"d9411",main:"9c551"}};
