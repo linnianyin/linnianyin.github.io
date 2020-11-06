7ae3ee3109b546824c2c03f6f88885eceec48223
sreed <youaresobad@foxmail.com>
2020-11-06 18:21:50 +0800

    add peekcard-mask

--- creator-cases/src/settings.f35d4.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","3d"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"279e0",main:"a124a"}};
